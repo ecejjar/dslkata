e2f86bcd3cc05d64a605a69131f66eab857f54da
ecejjar <jesus.javier.arauz@ericsson.com>
2018-12-20 16:38:50 +0100

    Solution added

---
+++ calculator3.groovy
@@ -0,0 +1,40 @@
+class Memory extends Stack<Double> {
+    Double value
+
+    void ms () {
+        push(value)
+    }
+
+    void mr () {
+        value = pop()
+    }
+}
+
+class Calculator {
+    double result
+    Memory mem = new Memory()
+
+    void sum ( double a ) {
+        result += a
+    }
+
+    void subs ( double a ) {
+        result -= a
+    }
+
+    void memorize ( Closure cl ) {
+        mem.value = result
+        def code = cl.rehydrate(mem, this, this)
+        code.resolveStrategy = Closure.DELEGATE_ONLY
+        code()
+        result = mem.value
+    }
+}
+
+def calculate ( Closure cl ) {
+    def calc = new Calculator()
+    def code = cl.rehydrate(calc, this, this)
+    code.resolveStrategy = Closure.DELEGATE_ONLY
+    code()
+}
+
